844c4506fbc086f6a21495916a287edaec36ed83
enhui zhu <zhuen2000@163.com>
2020-10-09 21:37:07 +0100

    udpate reducers

---
+++ src/store/reducers/productsInBasket.reducer.ts
@@ -0,0 +1,9 @@
+import { Product } from '../../interfaces/product.interface';
+
+const initialState: Product[] = [];
+
+export default (state = initialState, {type, payload}: any) => {
+  switch (type) {
+
+  }
+}
