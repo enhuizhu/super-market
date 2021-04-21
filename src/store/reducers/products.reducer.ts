d6c0c79b05f14b183512ea9b98cc98848ac3251b
Enhui Zhu <enhui.zhu@sky.uk>
2021-04-21 10:44:51 +0100

    add new products

--- src/store/reducers/products.reducer.ts
+++ src/store/reducers/products.reducer.ts
@@ -1,28 +1,22 @@
 import { Product } from '../../interfaces/product.interface';
-import { BUY_ONE_GET_ONE_FREE,  DISCOUNT_AFTER_THREE} from '../../constants/policies.constants';
+import { BUY_ONE_GET_ONE_FREE, THREE_FOR_TWO} from '../../constants/policies.constants';
 
 const initialState: Product[] = [
   {
     id: 1,
-    name: 'Green Tea',
-    code: 'GR1',
-    price: 3.11,
+    name: 'Apples',
+    code: 'apple',
+    price: 0.6,
     policy: BUY_ONE_GET_ONE_FREE,
     policyDescription: 'Buy One Get One Free'
   },
   {
     id: 2,
-    name: 'Strawberries',
-    code: 'SR1',
-    price: 5,
-    policy: DISCOUNT_AFTER_THREE,
-    policyDescription: 'buy 3 or more, price is £4.5',
-  },
-  {
-    id: 3,
-    name: 'Coffee',
-    code: 'CF1',
-    price: 11.23,
+    name: 'Oranges',
+    code: 'orange',
+    price: 0.25,
+    policy: THREE_FOR_TWO,
+    policyDescription: '3 for the price of 2 on',
   }
 ];
 
